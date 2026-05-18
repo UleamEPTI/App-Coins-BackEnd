a666af13bc161e3f3ac353512ee5be4b2ed5c1d2
Israelfas <marcelomejiauep@gmail.com>
2026-05-18 16:40:49 -0500

    fix: corregido mapeo de entidad premios con postgres y endpoints probados con exito

--- src/premios/entities/premio.entity.ts
+++ src/premios/entities/premio.entity.ts
@@ -11,10 +11,10 @@ export class Premio {
   @Column({ nullable: true })
   descripcion: string;
 
-  @Column({ nullable: true })
+  @Column({ name: 'imagen_url', nullable: true }) // Mapeado correctamente a la columna física de la BD
   imagen: string;
 
-  @Column({ name: 'costo_puntos' })
+  @Column({ name: 'puntos_requeridos' }) // Traduce 'costo_puntos' en el código a 'puntos_requeridos' en Postgres
   costo_puntos: number;
 
   @Column({ default: 0 })
